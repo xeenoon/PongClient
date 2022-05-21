ccc74186bb15e5176615b165265f64e55528c24e
Christopher <Christopher@marvica.com.au>
2022-05-21 18:57:27 +1000

    Did a thing?

---
+++ PongClient/Networking.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PongClient
+{
+    class Networking
+    {
+        private static readonly Socket ClientSocket = new Socket
+            (AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+
+        private const int PORT = 9999;
+
+        public static void Setup()
+        {
+        //    Console.Title = "Client";
+            LoopConnect();
+            RequestLoop();
+        }
+
+        private static void LoopConnect()
+        {
+            int attempts = 0;
+
+            while (!ClientSocket.Connected)
+            {
+                try
+                {
+                    attempts++;
+                    // Change IPAddress.Loopback to a remote IP to connect to a remote host.
+                    ClientSocket.Connect("192.168.1.4", PORT);
+                }
+                catch
+                {
+                }
+            }
+        }
+
+        private static void RequestLoop()
+        {
+            while (true)
+            {
+                SendMoveID(1);
+                ReceiveResponse();
+            }
+        }
+
+        /// <summary>
+        /// Close socket and exit program.
+        /// </summary>
+        private static void Exit()
+        {
+            ClientSocket.Shutdown(SocketShutdown.Both);
+            ClientSocket.Close();
+            Environment.Exit(0);
+        }
+
+        /// <summary>
+        /// Sends a string to the server with ASCII encoding.
+        /// </summary>
+        private static void SendMoveID(byte ID)
+        {
+            byte[] buffer = new byte[1] {ID};
+            ClientSocket.Send(buffer, 0, buffer.Length, SocketFlags.None);
+        }
+
+        private static void ReceiveResponse()
+        {
+            var buffer = new byte[2048];
+            int received = ClientSocket.Receive(buffer, SocketFlags.None);
+            if (received == 0)
+            {
+                return;
+            }
+            var data = new byte[received];
+            Array.Copy(buffer, data, received);
+            string text = Encoding.ASCII.GetString(data);
+            Console.WriteLine(text);
+        }
+    }
+}
